04cbab81d5ce0a3eea403f9ad27f0007e3d16f2f
marielaveron <maravsf@gmail.com>
2023-08-13 18:06:42 -0300

    Solución al problema del div

--- eventos.js
+++ eventos.js
@@ -1,5 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     const soyDiv = document.querySelector("div");
+    const btn = document.querySelector("button");
+
+    // Solución al problema donde el evento click del div también se disparaba.
+    // Saque el onclick para mayor comodidad, y agrege el evento al js
+    // Implemente el stopPropagation(), para que no siguiera activandose el evento del div.
+
+    btn.addEventListener("click", (event) => {
+        alert('Hola!');
+        event.stopPropagation();
+    });
 
     soyDiv.addEventListener("click", () => {
         alert('Hola! Soy el div');
