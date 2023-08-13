4c0af282945d94c0ee7a63e1369aa338b0830ef6
marielaveron <maravsf@gmail.com>
2023-08-13 17:22:13 -0300

    Agrege evento al Div

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const soyDiv = document.querySelector("div");
+
+    soyDiv.addEventListener("click", () => {
+        alert('Hola! Soy el div');
+});
+});
